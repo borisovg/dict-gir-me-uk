782f936476350c5227358f95058701979166280b
George Borisov <git@gir.me.uk>
2022-09-20 23:12:19 +0100

    feat: tweak xetex

--- templates/word_xetex_russian.php
+++ templates/word_xetex_russian.php
@@ -3,10 +3,10 @@
 $code = <<<XELATEX
 % {$WORD_DATA['russian']}
 \\begin{longtable}{
-    >{\\raggedright}p{37mm}@{\\hspace{2mm}}
-    >{\\raggedright}p{24mm}@{\\hspace{2mm}}|
+    >{\\raggedright}p{35mm}@{\\hspace{2mm}}
+    >{\\raggedright}p{30mm}@{\\hspace{2mm}}|
     p{2mm}
-    >{\\raggedright}p{30mm}@{\\hspace{2mm}}
+    >{\\raggedright}p{33mm}@{\\hspace{2mm}}
     >{\\raggedleft}p{31mm}
 }
 {\\textbf{\\large {$WORD_DATA['r1c1']}\index{{$WORD_DATA['index1']} -- {$WORD_DATA['index2']}}}} {$WORD_DATA['type_r']} &
@@ -30,12 +30,12 @@ XELATEX;
 
 $code .= <<<XELATEX
 \\multicolumn{2}{
-    >{\\RaggedRight}p{63mm}@{\\hspace{2mm}}|
+    >{\\RaggedRight}p{70mm}@{\\hspace{2mm}}|
 }
 {\\textit{\\small {$WORD_DATA['r3c1']}}} &
 \multicolumn{1}{ c }{} &
 \multicolumn{2}{
-    >{\\RaggedRight}p{63mm}
+    >{\\RaggedRight}p{68mm}
 }
 {\\textit{\\small {$WORD_DATA['r3c3']} {$WORD_DATA['rigveda']}}}
 \\tabularnewline
